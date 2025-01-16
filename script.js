fb13aa5e0f5e5de94970625e7003fc6edf19ebc4
atoinonen <35708618+atoinonen@users.noreply.github.com>
2025-01-17 00:57:07 +0200

    Add rest of the info to cards
    Adds rest of the information of the book to the cards: author, pages,
    and whether the book has been read.

--- script.js
+++ script.js
@@ -20,9 +20,30 @@ function listLibrary(library = myLibrary) {
     for (var book of library) {
         const card = document.createElement("div");
         card.className = "card"
+
+        const read = document.createElement("span");
+        read.textContent = book.read ? "☑" : "☐";
+        read.className = "checkmark";
+        card.appendChild(read);
+        
+        if (book.read){
+            card.className += " read"
+        }else{
+            card.className += " not-read"
+        }
+
         const title = document.createElement("h2");
         title.textContent = book.title;
         card.appendChild(title);
+
+        const author = document.createElement("h5");
+        author.textContent = "by: " + book.author;
+        card.appendChild(author);
+
+        const pages = document.createElement("p");
+        pages.textContent = book.pages + " pages";
+        card.appendChild(pages);
+
         list.appendChild(card);
     }
 }
